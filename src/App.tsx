dc34e63266a41e8c343dd72e723b7b23a9c192d7
bimaaghafara <bimaa7@gmail.com>
2025-07-12 00:16:47 +0700

    extracting the styles

--- src/App.tsx
+++ src/App.tsx
@@ -26,76 +26,78 @@ type Repo = {
 
 type ReposRes = Repo[];
 
+const styles = {
+  userCard: {
+    border: "1px solid",
+    margin: "12px 0",
+    padding: "12px",
+  },
+  userHeader: {
+    display: "flex",
+    justifyContent: "space-between",
+    cursor: "pointer",
+  },
+  arrow: (isExpanded: boolean): React.CSSProperties => ({
+    display: "inline-block",
+    transform: isExpanded ? "rotate(180deg)" : "rotate(0deg)",
+    transition: "transform 0.3s ease",
+  }),
+  repoList: {
+    maxHeight: "50vh",
+    overflow: "auto",
+    marginTop: "6px",
+  },
+  repoCard: {
+    margin: "2px 0",
+    padding: "12px",
+    background: "#eee",
+  },
+  textInfo: {
+    margin: "12px 0",
+  },
+  appContainer: {},
+  searchBar: {
+    display: "flex",
+    gap: "8px",
+  },
+};
+
 const UserItem = ({ user }: UserItemProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  // https://api.github.com/users/bimaaghafara/repos?page=1&per_page=5
   const [url, setUrl] = useState("");
-  const reposFetch = useFetch<ReposRes>({
-    url,
-  });
+  const reposFetch = useFetch<ReposRes>({ url });
 
   useEffect(() => {
-    if (isExpanded) {
-      setUrl(`https://api.github.com/users/${user.login}/repos`);
-    } else {
-      // reset url when not expanded, so will refetch when re-expanded content
-      setUrl("");
-    }
+    // set url when isExpanded is true to fetch user's repos
+    // reset url when isExpanded is false so will refetch when expanded again
+    setUrl(
+      isExpanded ? `https://api.github.com/users/${user.login}/repos` : ""
+    );
   }, [isExpanded]);
 
   const renderContent = () => {
     if (reposFetch.isLoading)
-      return <div style={{ margin: "12px 0" }}>Loading....</div>;
-    if (reposFetch.error)
-      return <div style={{ margin: "12px 0" }}>Error...</div>;
+      return <div style={styles.textInfo}>Loading....</div>;
+    if (reposFetch.error) return <div style={styles.textInfo}>Error...</div>;
     if (reposFetch.data?.length)
       return reposFetch.data.map((repo) => (
-        <div
-          key={repo.id}
-          style={{ margin: "2px 0", padding: "12px", background: "#eee" }}
-        >
+        <div key={repo.id} style={styles.repoCard}>
           <div>
             {repo.name} ({repo.stargazers_count}*)
           </div>
           <div>{repo.description || "-"}</div>
         </div>
       ));
-    return <div style={{ margin: "12px 0" }}>This user has no repository.</div>;
+    return <div style={styles.textInfo}>This user has no repository.</div>;
   };
 
   return (
-    <div
-      key={user.id}
-      style={{
-        border: "1px solid",
-        margin: "12px 0",
-        padding: "12px",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          cursor: "pointer",
-        }}
-        onClick={() => setIsExpanded(!isExpanded)}
-      >
+    <div key={user.id} style={styles.userCard}>
+      <div style={styles.userHeader} onClick={() => setIsExpanded(!isExpanded)}>
         <div>{user.login}</div>
-        <div
-          style={{
-            display: "inline-block",
-            transform: isExpanded ? "rotate(180deg)" : "rotate(0deg)",
-            transition: "transform 0.3s ease",
-          }}
-        >
-          V
-        </div>
+        <div style={styles.arrow(isExpanded)}>V</div>
       </div>
-      {isExpanded && (
-        <div style={{ maxHeight: "50vh", overflow: "auto", marginTop: "6px" }}>
-          {renderContent()}
-        </div>
-      )}
+      {isExpanded && <div style={styles.repoList}>{renderContent()}</div>}
     </div>
   );
 };
@@ -104,9 +106,7 @@ function App() {
   const [search, setSearch] = useState("");
   const [lastSearch, setLastSearch] = useState("");
   const [url, setUrl] = useState("");
-  const usersFetch = useFetch<UsersRes>({
-    url,
-  });
+  const usersFetch = useFetch<UsersRes>({ url });
 
   const handleClickSearch = () => {
     if (search) {
@@ -127,13 +127,12 @@ function App() {
   const renderContent = () => {
     if (!lastSearch) return null;
     if (usersFetch.isLoading)
-      return <div style={{ margin: "12px 0" }}>Loading....</div>;
-    if (usersFetch.error)
-      return <div style={{ margin: "12px 0" }}>Error...</div>;
+      return <div style={styles.textInfo}>Loading....</div>;
+    if (usersFetch.error) return <div style={styles.textInfo}>Error...</div>;
     if (usersFetch.data?.items?.length)
       return (
         <>
-          <div style={{ margin: "12px 0" }}>
+          <div style={styles.textInfo}>
             Showing results for <i>{lastSearch}</i>:
           </div>
           {usersFetch.data.items.map((user) => (
@@ -142,21 +141,23 @@ function App() {
         </>
       );
     return (
-      <div style={{ margin: "12px 0" }}>
+      <div style={styles.textInfo}>
         No Results Found for <i>{lastSearch}</i>
       </div>
     );
   };
 
   return (
-    <div>
-      <div>
+    <div style={styles.appContainer}>
+      <div style={styles.searchBar}>
         <input
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           onKeyDown={handleKeyDownInputSearch}
         />
-        <button onClick={handleClickSearch}>search</button>
+        <button onClick={handleClickSearch} disabled={!search}>
+          Search
+        </button>
       </div>
       {renderContent()}
     </div>
@@ -164,14 +165,3 @@ function App() {
 }
 
 export default App;
-
-/**
- *
- * todo:
- * - styling
- * - readme
- * - deployement
- * - error
- * - unit test
- *
- *  */
